5c84134c9cf854fa845d2eb0d6e92ab5e598e144
Nadiaaeputri <146184028+Nadiaaeputri@users.noreply.github.com>
2024-04-06 16:01:16 +0700

    Add files via upload

---
+++ model.php
@@ -0,0 +1,24 @@
+<?php
+class Contact {
+
+    //method untuk melakukan select
+    public static function select() {
+        
+    }
+
+    //method untuk melakukan insert
+    public static function insert() {
+
+    }
+
+    //method untuk melakukan delete
+    public static function delete() {
+
+    }
+
+    //method untuk melakukan update
+    public static function update($id) {
+
+    }
+}
+?>
